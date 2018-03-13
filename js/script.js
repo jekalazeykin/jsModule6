6e147be172b7b0b4e3510d73b00156e7f57a5bc3
E.Lazeykin <lazejjkin@i.ua>
2018-03-13 20:37:05 +0200

    module-6

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const keyTrainer = {
+  chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
+  charCount() {
+    while (true) {
+      this.number = Number(prompt('Введите число символов клавиатуры'));
+      if (this.number > 0 && Number.isInteger(this.number)) break;
+      alert('неверный ввод,введите целое положительное число')
+    }
+  },
+  task() {
+    this.arr = [];
+    for (let i = 0; i < this.number; i++) {
+      const char = this.chars[Math.floor(Math.random() * this.chars.length)];
+      this.arr.push(char);
+    }
+    return this.arr;
+  },
+  userInput() {
+    this.start = (prompt('Наберите строку ' + (this.arr))).split(",");
+    return this.start;
+  },
+  userErrors() {
+    let mistake = 0;
+    let on = 0;
+    for ( let i = 0; i < this.arr.length; i++) {
+      if (this.arr[i] === this.start[i]) {
+        on++
+      } else {
+        mistake++;
+      }
+    }
+    console.log(mistake);
+    return on == this.arr.length ? alert('Вы справились с заданием') : alert(`Вы совершили - ${mistake} ошибок. Успехов в последующих упражнениях!`);
+
+  }
+}
+
+function run() {
+  keyTrainer.charCount();
+  keyTrainer.task();
+  keyTrainer.userInput();
+  keyTrainer.userErrors();
+};
+
+run();
